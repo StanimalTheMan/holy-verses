b8ccbc8104ca2940be446cb5fa0a216b19b9189a
StanimalTheMan <sjc591@nyu.edu>
2023-01-02 17:59:06 -0500

    Initially attempt to have a more uniform flashcard clickable interface like Quizlet

--- src/components/Verse.js
+++ src/components/Verse.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 import styles from "./styles.module.css";
 
 const Verse = ({book, chapter, verses, content}) => {
@@ -16,14 +20,21 @@ const Verse = ({book, chapter, verses, content}) => {
         // e.g. verses = [25, 26, 27] -> 25-27
         versesFormatted = `${verses[0]}-${verses[verses.length - 1]}`;
     }
-    console.log(book, chapter, verses, content)
     return (
-        <>
-            {isVerseRevealed ? 
-            <button onClick={handleClick} className={styles.verse}>
-                {content}
-            </button> : <button onClick={handleClick}>{book} {chapter}:{versesFormatted}</button>}
-        </>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea onClick={handleClick}>
+                <CardContent>
+                    {isVerseRevealed ? 
+                        <Typography variant="body2" color="text.secondary">
+                            {content}
+                        </Typography> :
+                        <Typography gutterBottom variant="h5" component="div">
+                        {book} {chapter}:{versesFormatted}
+                        </Typography>
+                    }
+                </CardContent>
+            </CardActionArea>
+        </Card>
     )
 };
 
